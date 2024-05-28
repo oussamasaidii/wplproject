74cd5eb00aa1a948b9305765c16031052bb2adc9
Thoelen Bilal (s140178) <bilal.thoelen@student.ap.be>
2024-05-28 18:47:17 +0000

    charachters api

--- public/Js/characters.js
+++ public/Js/characters.js
@@ -1,9 +1,15 @@
 fetch('https://fortnite-api.com/v2/cosmetics/br/search/all?type=outfit&api_key=91adadf6-2fef-446b-8455-e5bdd97894ab')
     .then(response => response.json())
     .then(data => {
-        const element = document.getElementById('myElement');
-        console.log(data)
-        data.data.slice(0, 30).forEach(item => {
+        const characterContainer = document.getElementById('character-container');
+        console.log(data);
+
+        //Filter voor Npc en null data
+        const filteredData = data.data.filter(item => {
+            return item.name && item.name.toLowerCase() !== 'npc' && item.name.toLowerCase() !== 'null';
+        });
+
+        filteredData.slice(0, 56).forEach(item => {
             const characterArticle = document.createElement('article');
             characterArticle.classList.add('character');
             const img = document.createElement('img');
@@ -13,6 +19,7 @@ fetch('https://fortnite-api.com/v2/cosmetics/br/search/all?type=outfit&api_key=9
             p.textContent = item.name;
             characterArticle.appendChild(img);
             characterArticle.appendChild(p);
-            element.appendChild(characterArticle);
+            characterContainer.appendChild(characterArticle);
         });
-    });
+    })
+    .catch(error => console.error('Error fetching data:', error));
